1cdab022a918e324f50731279a77dc2de70299ea
Arthur Teixeira <arthurteixeiracl@email.com>
2026-03-24 16:08:43 -0300

    subindo para uma branch diferente

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 # quero testar o pull e vou criar um arquivo py direto no meu repositorio remoto
+print("Hello, estou treinando git e seus comandos. esse prompt ficará em uma branch chamada 'test'")
+a=b=4
+print(a+b)
